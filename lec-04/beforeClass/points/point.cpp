bb9833ba86c66c6a86d5d8c9d243ff440bae8401
Diba Mirza <dimirza@cs.ucsb.edu>
2018-04-10 21:49:17 -0700

    code for lecture

---
+++ lec-04/beforeClass/points/point.cpp
@@ -0,0 +1,30 @@
+/ FILE: point.cpp
+// CLASS IMPLEMENTED: point (See point.h for documentation.)
+
+#include "point.h"
+
+
+point::point(double initial_x, double initial_y)
+{
+    x = initial_x;   // Constructor sets the point to a given position.
+    y = initial_y;
+}
+
+
+void point::shift(double x_amount, double y_amount)
+{
+    x += x_amount;
+    y += y_amount;   
+}
+
+
+void point::rotate90( )
+{
+    double new_x;
+    double new_y;
+
+    new_x = y;  // For a 90 degree clockwise rotation, the new x is the original y,
+    new_y = -x; // and the new y is -1 times the original x.
+    x = new_x;
+    y = new_y; 
+}
