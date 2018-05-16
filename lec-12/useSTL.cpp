63ded7e88a527b4270fb1457fe0409370b62a3c0
Diba Mirza <dimirza@cs.ucsb.edu>
2018-05-15 20:32:18 -0700

    lec 12

--- lec-12/useSTL.cpp
+++ lec-12/useSTL.cpp
@@ -46,25 +46,4 @@ int main() {
     cout<<endl;
 
 
-    // insert data to bst1
-    /*bst1.insert(64);
-    bst1.insert(128);
-    bst1.insert(8);
-    bst1.insert(512);
-    bst1.insert(256);
-    bst1.insert(32);
-    bst1.insert(16);
-    bst1.insert(4);
-    cout<<"Inserted\n64 128 8 512 256 32 16 4 "<<endl;
-    cout<<"In order traversal: \n";
-    bst1.printInOrder();
-
-
-    BST<std::string> bst2;
-
-    // insert data to bst1
-
-    cout<<"In order traversal: \n";
-    bst2.printInOrder();*/
-
 }
